f49fcf70c69fb1a882db079e8f1a5091a37d5b36
Suman Paul <MeSumanPaul@users.noreply.github.com>
2022-05-08 02:54:32 +0530

    Google form added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,3 +17,34 @@ $(ready(function(){
 
 }));
 
+
+let url = "https://docs.google.com/forms/d/e/1FAIpQLSdVSiHdFSVeP2804xUcTQAKN8KeASJvWSnU0dHxUZBs0UF_fw/formResponse"; //action url
+let form = document.querySelector("#form"); //form element
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); //prevent default behaviour
+
+  fetch(url, {
+      method: "POST",
+      mode: "no-cors",
+      header: {
+        'Content-Type': 'application/json'
+      },
+      body: getInputData()
+    })
+    .then(data => {
+      console.log(data);
+      alert("Form Submitted");
+    })
+    .catch(err => console.error(err));
+});
+
+//populating input data
+function getInputData() {
+  let dataToPost = new FormData(); //formdata API
+
+  //fill name attributes to corresponding values
+  dataToPost.append("ContactName", document.querySelector("#input1").value);
+
+  return dataToPost;
+}
